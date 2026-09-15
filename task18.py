a2ff05049b33ea3fec375b00fcfb45d2c9b68a3a
Klerik3x <klerik3x@yandex.ru>
2026-09-15 20:52:47 +0300

    task18.py

---
+++ task18.py
@@ -0,0 +1,20 @@
+# #todo: Заданы множества
+# #Даны читатели книг
+# readers_books = {'id3', 'id5', 'id9', 'id8', 'id2', 'id1' }
+#
+# #Даны читатели газет
+# readers_magazines = { 'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
+#
+# Найти пользователей кто читает и книги и газеты
+
+# Читатели книг
+readers_books = {'id3', 'id5', 'id9', 'id8', 'id2', 'id1'}
+
+# Читатели газет
+readers_magazines = {'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
+
+# Пересечение: кто читает И книги, И газеты
+all = readers_books & readers_magazines
+
+print("Читают и книги, и газеты:")
+print(all)
